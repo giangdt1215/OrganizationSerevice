afcb9288ac76031bfce94865320faaf80ddfb8a7
Dang Giang <gianggiang1512@gmail.com>
2022-06-30 23:17:03 +0700

    Use ThreadLocal for fields and change value for AUTH_TOKEN field

--- src/main/java/com/optimagrowth/organizationservice/utils/UserContext.java
+++ src/main/java/com/optimagrowth/organizationservice/utils/UserContext.java
@@ -1,49 +1,57 @@
 package com.optimagrowth.organizationservice.utils;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserContext {
 
     public static final String CORRELATION_ID = "tmx-correlation-id";
-    public static final String AUTH_TOKEN     = "tmx-auth-token";
+    public static final String AUTH_TOKEN     = "Authorization";
     public static final String USER_ID        = "tmx-user-id";
     public static final String ORG_ID         = "tmx-org-id";
 
-    private String correlationId;
-    private String authToken;
-    private String userId;
-    private String orgId;
+    private static final ThreadLocal<String> correlationId = new ThreadLocal<>();
+    private static final ThreadLocal<String> authToken = new ThreadLocal<>();
+    private static final ThreadLocal<String> userId = new ThreadLocal<>();
+    private static final ThreadLocal<String> orgId = new ThreadLocal<>();
 
-    public String getCorrelationId() {
-        return correlationId;
+    public static HttpHeaders getHttpHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(CORRELATION_ID, getCorrelationId());
+
+        return httpHeaders;
     }
 
-    public void setCorrelationId(String correlationId) {
-        this.correlationId = correlationId;
+    public static String getCorrelationId() {
+        return correlationId.get();
     }
 
-    public String getAuthToken() {
-        return authToken;
+    public static void setCorrelationId(String cid) {
+        correlationId.set(cid);
     }
 
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
+    public static String getAuthToken() {
+        return authToken.get();
     }
 
-    public String getUserId() {
-        return userId;
+    public static void setAuthToken(String aToken) {
+        authToken.set(aToken);
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public static String getUserId() {
+        return userId.get();
     }
 
-    public String getOrgId() {
-        return orgId;
+    public static void setUserId(String aUser) {
+        userId.set(aUser);
     }
 
-    public void setOrgId(String orgId) {
-        this.orgId = orgId;
+    public static String getOrgId() {
+        return orgId.get();
+    }
+
+    public static void setOrgId(String aOrg) {
+        orgId.set(aOrg);
     }
 }
