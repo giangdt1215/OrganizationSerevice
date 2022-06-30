ff27cbe1511bb8630452600b3736f0a79f7da007
Dang Giang <gianggiang1512@gmail.com>
2022-06-30 23:19:07 +0700

    Add Spring Cloud Stream Kafka to publish messages

--- src/main/java/com/optimagrowth/organizationservice/service/OrganizationService.java
+++ src/main/java/com/optimagrowth/organizationservice/service/OrganizationService.java
@@ -1,5 +1,7 @@
 package com.optimagrowth.organizationservice.service;
 
+import com.optimagrowth.organizationservice.events.source.ActionEnum;
+import com.optimagrowth.organizationservice.events.source.SimpleSourceBean;
 import com.optimagrowth.organizationservice.model.Organization;
 import com.optimagrowth.organizationservice.repository.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,23 +16,34 @@ public class OrganizationService {
     @Autowired
     private OrganizationRepository repository;
 
+    @Autowired
+    private SimpleSourceBean simpleSourceBean;
+
     public Organization findById(String organizationId) {
         Optional<Organization> opt = repository.findById(organizationId);
+        //publish message
+        simpleSourceBean.publishOrganizationChange(ActionEnum.GET, organizationId);
         return (opt.isPresent()) ? opt.get() : null;
     }
 
     public Organization create(Organization organization){
         organization.setId(UUID.randomUUID().toString());
         organization = repository.save(organization);
+        //publish message
+        simpleSourceBean.publishOrganizationChange(ActionEnum.CREATED, organization.getId());
         return organization;
 
     }
 
     public void update(Organization organization){
         repository.save(organization);
+        //publish message
+        simpleSourceBean.publishOrganizationChange(ActionEnum.UPDATED, organization.getId());
     }
 
     public void delete(String organizationId){
         repository.deleteById(organizationId);
+        //publish message
+        simpleSourceBean.publishOrganizationChange(ActionEnum.DELETED, organizationId);
     }
 }
